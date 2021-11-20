1bd416797278ea43ad2ef84c56ef7d9df65fc87c
gianmarco-murgia-dev <gianmarco.murgia.dev@gmail.com>
2021-11-20 10:23:32 +0100

    First commit

---
+++ app/Views/boxModel.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Box model</title>
+
+    <link rel="stylesheet" href="styles/boxModel.css">
+</head>
+<body>
+    <div id="box1">
+
+    </div>
+    <div id="box2">
+
+    </div>
+    <div id="box3">
+
+    </div>
+</body>
+</html>
